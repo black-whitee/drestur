a1ce499d9a0328a9834570f81a83ffe90e1de01d
401 <401@academicos.cesde.edu.co>
2022-11-11 12:22:39 -0500

    se crea proyecto

---
+++ src/main/java/clases/empleado.java
@@ -0,0 +1,14 @@
+package clases;
+
+public class empleado {
+    private  String nombre;
+    private  String apellido;
+    private String id;
+    private int edad;
+    private boolean salario;
+    private  String documento;
+    private String pasaporte;
+    private  String visa;
+
+
+}
---
+++ src/main/java/clases/hospedaje.java
@@ -0,0 +1,17 @@
+package clases;
+
+public class hospedaje {
+
+    //atributos
+    private String hotel;
+    private int casa;
+    private double comida;
+    private double bebida;
+    private String guia;
+
+
+
+
+
+
+}
